d2b0b4b949f2378ff3a40ea3cb2d75e56746408f
ygor <ygortavela@gmail.com>
2019-09-16 20:34:31 -0300

    aaaaaA

--- mac422shell.c
+++ mac422shell.c
@@ -1,6 +1,7 @@
 #include <unistd.h>
 #include <signal.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -8,37 +9,8 @@
 #define TRUE 1
 #define BUFFER 64
 
-char *read_command( void);
-char **parse_command( char *input);
-
-int main( int argc, char *argv[])
-{
-    char *input;
-    char **command;
-    pid_t child_pid;
-    pid_t wait_response;
-    int stat_loc;
-
-    while (TRUE) {
-        input = read_command();
-        command = parse_command( input);
-
-        child_pid = fork();
-
-        if (child_pid == 0) {
-            execvp( command[0], command);
-        } else {
-            wait_response = waitpid( child_pid, &stat_loc, WUNTRACED);
-        }
-
-        free( input);
-        free( command);
-    }
-
-    return 0;
-}
-
-char *read_command( void)
+char *
+read_command( void)
 {
     char *input = malloc( BUFFER * sizeof (char));
 
@@ -48,7 +20,8 @@ char *read_command( void)
     return input;
 }
 
-char **parse_command( char *input)
+char **
+parse_command( char *input)
 {
     char **commmand = malloc( strlen( input) * sizeof (char *));
     char *parsed = strtok( input, " ");
@@ -62,4 +35,91 @@ char **parse_command( char *input)
     commmand[index] = NULL;
 
     return commmand;
+}
+
+void
+protegepracaramba( char *argv[])
+{
+    chmod( argv[1], 0000);
+}
+
+void
+liberageral( char *argv[])
+{
+    chmod( argv[1], 0777);
+}
+
+void
+rodeveja( char *argv[])
+{
+    pid_t process_pid;
+    pid_t wait_response;
+    int stat_loc;
+    
+    process_pid = fork();
+
+    if (process_pid == -1) {
+        perror("fork() failed!");
+        _exit(1);
+    } else if (process_pid == 0) {
+        execve( argv[1], NULL, NULL);
+    } else {
+        wait_response = waitpid( process_pid, &stat_loc, WUNTRACED);
+        printf("=> programa '%s' retornou com codigo %d.\n", argv[1], stat_loc);
+    }
+}
+
+void
+rode( char *argv[])
+{
+    pid_t process_pid;
+
+    process_pid = fork();
+
+    if (process_pid == -1) {
+        perror("fork() failed!");
+        _exit(1);
+    } else if (process_pid == 0) {
+        setsid();
+        execve( argv[1], NULL, NULL);
+    } else {
+        signal( SIGCHLD, SIG_IGN);
+    }
+}
+
+int 
+main( int argc, char const *argv[])
+{
+    char *input;
+    char **command;
+    pid_t process_pid;
+    pid_t wait_response;
+    int stat_loc;
+
+    while (TRUE) {
+        input = read_command();
+        command = parse_command( input);
+
+        if (!strcmp( command[0], "exit")) {
+            free( input);
+            free( command);
+
+            break;
+        } else if (!strcmp( command[0], "protegepracaramba")) {
+            protegepracaramba( command); 
+        } else if (!strcmp( command[0], "liberageral")) {
+            liberageral( command); 
+        } else if (!strcmp( command[0], "rodeveja")) {
+            rodeveja( command); 
+        } else if (!strcmp( command[0], "rode")) {
+            rode( command); 
+        } else {
+            printf( "command not found: %s\n", command[0]);
+        }
+
+        free( input);
+        free( command);
+    }
+
+    return 0;
 }
