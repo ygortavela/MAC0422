8d154379b29e96b3c12ffd8276aae92562065b9a
Ygor Tavela <ygortavela@gmail.com>
2019-11-13 16:28:43 -0300

    aaaaaaa

--- EP3/usr/local/src/memorymap.c
+++ EP3/usr/local/src/memorymap.c
@@ -1,9 +1,6 @@
 /* ######################################################## */
 #include "/usr/src/servers/is/inc.h"
 #include "/usr/src/servers/pm/mproc.h"
-#include <stdio.h>
-#include <minix/config.h>
-#include <minix/type.h>
 
 int
 main( int argc, char *argv[])
@@ -18,17 +15,16 @@ main( int argc, char *argv[])
 
   printf("Memory map:\n<pid>\t<first pos>\t<last pos>\n");
 
-  for (i = 0; i < NR_PROCS; i++) {
+  for (i = 0; i < NR_PROCS; i++)
     if (mp[i].mp_pid != 0 || i == PM_PROC_NR)
       printf("%d\t0x%x\t\t0x%x\n", mp[i].mp_pid, mp[i].mp_seg[T].mem_phys, mp[i].mp_seg[S].mem_phys + mp[i].mp_seg[S].mem_len);
-  }
   
   mem_free = 0;
 
   for (i = 0; i < _NR_HOLES; i++)
     if (pmi.pmi_holes[i].h_base) mem_free += pmi.pmi_holes[i].h_len;
 
-  printf("Free memory (clicks): %d\n", mem_free);
+  printf("Free memory (kB): %d\n", (mem_free << CLICK_SHIFT)/1024);
 
   return 0;
 }
